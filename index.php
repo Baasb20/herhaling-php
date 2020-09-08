de26f50332c9392625e3f9ae4761a5fc77e72c05
Baasb20 <2058682@talnet.nl>
2020-09-08 17:55:17 +0200

    huiswerk

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+// ..........
+// $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+
+//Oefening 1
+//var_dump($klas);
+
+// Oefening 2
+// for($i=0; $i<count($klas); $i++){
+//   echo $klas[$i], PHP_EOL;
+// }
+
+// Oefening 3
+// foreach ($klas as $key => $value) {
+//   echo $klas[$key], PHP_EOL;
+// }
+
+// ...........
+
+
+//Combinatie opdracht
+
+// $bucket_list = [];
+//
+// $hoeveelheid_activiteiten = readline("Hoeveel item wil je toevoegen aan je bucket list?");
+//
+// for($i=0; $i<$hoeveelheid_activiteiten; $i++){
+//   $activiteit = readline ("Welke acticiteiten wil je toegvoegen?");
+//   array_push($bucket_list, $activiteit);
+//
+// }
+//
+// var_dump($bucket_list);
+//
+// for($i=0; $i<count($bucket_list); $i++){
+//    echo $bucket_list[$i], PHP_EOL;
+// }
+
+?>
